647b490012ba898486451affb0ddd8058463ae2e
nghia.nguyen-dinh <nghia.nguyen-dinh@banvien.com.vn>
2023-08-03 14:03:01 +0700

    modifier

--- src/main/java/com/nixagh/classicmodels/config/web_socket/WebSocketConfig.java
+++ src/main/java/com/nixagh/classicmodels/config/web_socket/WebSocketConfig.java
@@ -9,26 +9,36 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final String[] STOMP_ENDPOINT = {
+            "/ws",          // normal endpoint
+            "/admin/ws"     // admin endpoint
+    };
+
+    private final String[] MESSAGE_BROKER = {
+            "/topic",               // normal topic
+            "/admin/notification"   // admin topic
+    };
+
+    private final String[] DESTINATION_PREFIX = {
+            "/app",         // normal destination prefix
+            "/admin/app"    // admin destination prefix
+    };
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // register the endpoint and enable SockJS
-        registry.addEndpoint("/ws")
-                .withSockJS();
-
-        // admin endpoint
-        registry.addEndpoint("/admin/ws")
+        registry.addEndpoint(STOMP_ENDPOINT)
                 .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // endpoint for sending message
-        registry.setApplicationDestinationPrefixes("/app");
-
+        registry.setApplicationDestinationPrefixes(DESTINATION_PREFIX);
         // endpoint for subscribing to topic
-        registry.enableSimpleBroker("/topic");
-        registry.enableSimpleBroker("/admin/notification");
-        registry.setUserDestinationPrefix("/user");
+        registry.enableSimpleBroker(MESSAGE_BROKER);
+        // normal user destination prefix
+        String USER_DESTINATION_PREFIX = "/user";
+        registry.setUserDestinationPrefix(USER_DESTINATION_PREFIX);
     }
 }
